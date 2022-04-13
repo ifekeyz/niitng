3f86f9720f019ff232fcf22c4130fe6c2c27eb8d
unknown <you@example.com>
2022-04-13 10:42:40 -0700

    Added file

---
+++ staticfiles/js/loader.js
@@ -0,0 +1,7 @@
+// document.addEventListener("DOMContentLoaded", function(){
+//     document.querySelector(".popup").style.display = "block"
+// })
+
+// document.querySelector(".close").addEventListener("click", function(){
+//     document.querySelector(".popup").style.display = "none"
+// })
